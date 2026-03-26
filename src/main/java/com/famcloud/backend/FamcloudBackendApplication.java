a811ee704be8d8732dbc75e2af0f8044be1fa77c
yashadashetty07 <yash.adashetty@gmail.com>
2026-03-26 21:11:26 +0530

    day 1 completed:created springboot backend app and directories.Configured mysql connection and database created.test api controller successful

---
+++ src/main/java/com/famcloud/backend/FamcloudBackendApplication.java
@@ -0,0 +1,12 @@
+package com.famcloud.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamcloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamcloudBackendApplication.class, args);
+	}
+}
---
+++ src/main/java/com/famcloud/backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.famcloud.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "FamCloud Backend is Running 🚀";
+    }
+}
