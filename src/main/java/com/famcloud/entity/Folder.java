1d214f26b33ce62d2b2da67b498ff20ac46c4674
yashadashetty07 <yash.adashetty@gmail.com>
2026-04-29 22:09:47 +0530

    completed and tested code upto day 8: core folder entity and structure

--- src/main/java/com/famcloud/entity/Folder.java
+++ src/main/java/com/famcloud/entity/Folder.java
@@ -1,4 +1,39 @@
 package com.famcloud.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "folders")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false,name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Folder parent;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false,name = "is_family_folder")
+    private boolean isFamilyFolder;
+
+    @Column(nullable = false,name = "created_at")
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/famcloud/repository/FolderRepository.java
+++ src/main/java/com/famcloud/repository/FolderRepository.java
@@ -1,4 +1,14 @@
 package com.famcloud.repository;
 
-public class FolderRepository {
+import com.famcloud.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder,Long> {
+    List<Folder> findByUserId(Long userId);
+    List<Folder> findByParent_Id(Long parentId);
+
 }
